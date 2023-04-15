1643d25cc0890e49a673c2941176b1dc8b9558fb
luoyantao99 <49816287+luoyantao99@users.noreply.github.com>
2023-04-15 15:23:16 -0400

    Update matrix_multiplication_OpenMP.c

--- matrix_multiplication_OpenMP.c
+++ matrix_multiplication_OpenMP.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <time.h>
 
-#define N 1024
+/*
+compile
+gcc -fopenmp -Wall -std=c99 -o matrix_OpenMP matrix_multiplication_OpenMP.c
 
-void initialize_matrices(double *A, double *B, double *C) {
+execute
+time ./matrix_OpenMP <number_of_threads> <matrix_dimension>
+*/
+
+
+void initialize_matrices(double *A, double *B, double *C, int N) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             A[i * N + j] = rand() % 100;
@@ -14,15 +22,22 @@ void initialize_matrices(double *A, double *B, double *C) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: ./matrix_OpenMP <number_of_threads> <matrix_dimension>\n");
+        return 1;
+    }
+    int N = atoi(argv[2]);
+
     double *A = malloc(N * N * sizeof(double));
     double *B = malloc(N * N * sizeof(double));
     double *C = malloc(N * N * sizeof(double));
 
-    initialize_matrices(A, B, C);
+    initialize_matrices(A, B, C, N);
 
     double start_time = omp_get_wtime();
     
+    omp_set_num_threads(atoi(argv[1]));
     #pragma omp parallel for
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -33,7 +48,7 @@ int main() {
     }
 
     double end_time = omp_get_wtime();
-    printf("Execution time with OpenMP: %lf seconds\n", end_time - start_time);
+    printf("Matrix Multiplication Execution Time (OpenMP): %.3f seconds\n", end_time - start_time);
 
     free(A);
     free(B);
