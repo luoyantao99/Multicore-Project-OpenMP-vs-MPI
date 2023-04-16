d2b184aa58d6acbb869c77cc33b93b59ddcb53d4
yuchenzhao726 <102622455+yuchenzhao726@users.noreply.github.com>
2023-04-15 20:31:19 -0400

    Update thread_creation_MPI.c

--- thread_creation_MPI.c
+++ thread_creation_MPI.c
@@ -27,15 +27,13 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size != NUM_PROCESSES) {
-        if (rank == 0) {
-            printf("Error: Run the MPI program with %d processes using 'mpirun -np %d <executable>'\n", NUM_PROCESSES, NUM_PROCESSES);
-        }
-        MPI_Finalize();
-        return 1;
-    }
-
+    double start_time = MPI_Wtime();
     dummy_function();
+    double end_time = MPI_Wtime();
+    
+    if (rank == 0) {
+        printf("Execution time with MPI: %f seconds\n", end_time - start_time);
+    }
 
     MPI_Finalize();
     return 0;
