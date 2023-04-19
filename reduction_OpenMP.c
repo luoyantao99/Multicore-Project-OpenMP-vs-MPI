624e8b63946f175656f0b8df77f8187c3fa5d194
luoyantao99 <49816287+luoyantao99@users.noreply.github.com>
2023-04-19 15:11:07 -0400

    Update reduction_OpenMP.c

--- reduction_OpenMP.c
+++ reduction_OpenMP.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <time.h>
+#include <math.h>
 
 /*
 compile
-gcc -fopenmp -Wall -std=c99 -o reduction_OpenMP reduction_OpenMP.c
+gcc -fopenmp -Wall -std=c99 -o reduction_OpenMP reduction_OpenMP.c -lm
 
 execute
 time ./reduction_OpenMP <number_of_threads> <array_length>
 */
 
-void initialize_array(size_t *arr, int N) {
+void initialize_array(double *arr, int N) {
     for (int i = 0; i < N; i++) {
         arr[i] = i;
     }
@@ -24,8 +25,8 @@ int main(int argc, char *argv[]) {
     }
     int N = atoi(argv[2]);
 
-    size_t *arr = malloc(N * sizeof(size_t));
-    size_t sum = 0;
+    double *arr = malloc(N * sizeof(double));
+    double sum = 0;
 
     initialize_array(arr, N);
 
@@ -33,14 +34,14 @@ int main(int argc, char *argv[]) {
 
     omp_set_num_threads(atoi(argv[1]));
     #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < N; ++i) {
-        sum += arr[i];
+    for (int i = 0; i < N; i++) {
+        sum += (sin(arr[i]) + cos(arr[i]));
     }
 
     double end_time = omp_get_wtime();
-    // printf("Execution time with OpenMP: %f seconds\n", end_time - start_time);
     printf("%f\n", end_time - start_time);
-    // printf("Sum: %zu\n", sum);
+    // printf("Execution time with OpenMP: %f seconds\n", end_time - start_time);
+    // printf("Sum: %f\n", sum);
 
     free(arr);
     return 0;
